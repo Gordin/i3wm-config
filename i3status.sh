39f391e47e34a0999bce5bb74353c22c0a5aaf63
Gordin <9ordin@gmail.com>
2012-10-29 23:51:44 +0100

    added network throughput to eth0 and wlan0

--- i3status.sh
+++ i3status.sh
@@ -7,28 +7,62 @@
 # showing cpu_usage has to be enabled (because ram_usage is prepended to that with sed...)
 # output_format has to be set to "i3bar"
 
+ETHR1=`cat /sys/class/net/eth0/statistics/rx_bytes`
+ETHT1=`cat /sys/class/net/eth0/statistics/tx_bytes`
+WLANR1=`cat /sys/class/net/wlan0/statistics/rx_bytes`
+WLANT1=`cat /sys/class/net/wlan0/statistics/tx_bytes`
 i3status | while :
 do
-        read line
+    read line
 
-        # get free and used RAM (this excludes cache)
-        ram_both=`free -k | sed -n -e '3 p' | sed 's/-\/+ buffers\/cache:\s*//'`
-        used=`echo $ram_both | sed "s/\([0-9]*\)\s*\([0-9]*\)/\1/"`
-        free=`echo $ram_both | sed "s/\([0-9]*\)\s*\([0-9]*\)/\2/"`
-        used=`echo "scale=1; $used / 1024 / 1024" | bc`
-        free=`echo "scale=1; $free / 1024 / 1024" | bc`
-
-        # if free RAM is less than 2GB show text in red, else green
-        if [ $(echo "$free < 2" | bc) -eq 1 ]
+    if [ `cat /sys/class/net/eth0/operstate` == "up" ]
         then
-            color=FF0000
-        else
-            color=00FF00
-        fi
-        
-        # put ram_usage before cpu_usage
-        sedline="s/cpu_usage/ram_usage\",\"color\":\"#${color}\",\"full_text\":\"RAM: ${used}G used ${free}G free\"},{\"name\":\"cpu_usage/"
-        final=`echo $line | sed -e "$sedline" | sed -e "s/% \"/%\"/"`
+            ETHR2=`cat /sys/class/net/eth0/statistics/rx_bytes`
+            ETHT2=`cat /sys/class/net/eth0/statistics/tx_bytes`
+            ETHTBPS=`expr $ETHT2 - $ETHT1`
+            ETHRBPS=`expr $ETHR2 - $ETHR1`
+            ETHTKBPS=`expr $ETHTBPS / 1024`
+            ETHRKBPS=`expr $ETHRBPS / 1024`
 
-        echo "$final" || exit 1
+            ETHR1=$ETHR2
+            ETHT1=$ETHT2
+            sedeth="s/\(\"name\":\"ethernet\",\"instance\":\"eth0\".*)\)\(\"}\)/\1 D:$ETHRKBPS U:$ETHTKBPS\2/"
+            line=`echo $line | sed -e "$sedeth"`
+    fi
+
+    if [ `cat /sys/class/net/wlan0/operstate` == "up" ]
+        then
+            WLANR2=`cat /sys/class/net/wlan0/statistics/rx_bytes`
+            WLANT2=`cat /sys/class/net/wlan0/statistics/tx_bytes`
+            WLANTBPS=`expr $WLANT2 - $WLANT1`
+            WLANRBPS=`expr $WLANR2 - $WLANR1`
+            WLANTKBPS=`expr $WLANTBPS / 1024`
+            WLANRKBPS=`expr $WLANRBPS / 1024`
+
+            WLANR1=$WLANR2
+            WLANT1=$WLANT2
+            sedwlan="s/\(\"name\":\"wireless\",\"instance\":\"wlan0\".*)\)\(\"}\)/\1 D:$WLANRKBPS U:$WLANTKBPS\2/"
+            line=`echo $line | sed -e "$sedwlan"`
+    fi
+
+    # get free and used RAM (this excludes cache)
+    ram_both=`free -k | sed -n -e '3 p' | sed 's/-\/+ buffers\/cache:\s*//'`
+    used=`echo $ram_both | sed "s/\([0-9]*\)\s*\([0-9]*\)/\1/"`
+    free=`echo $ram_both | sed "s/\([0-9]*\)\s*\([0-9]*\)/\2/"`
+    used=`echo "scale=1; $used / 1024 / 1024" | bc`
+    free=`echo "scale=1; $free / 1024 / 1024" | bc`
+
+    # if free RAM is less than 2GB show text in red, else green
+    if [ $(echo "$free < 2" | bc) -eq 1 ]
+    then
+        color=FF0000
+    else
+        color=00FF00
+    fi
+
+    # put ram_usage before cpu_usage
+    sedline="s/cpu_usage/ram_usage\",\"color\":\"#${color}\",\"full_text\":\"RAM: ${free}G\"},{\"name\":\"cpu_usage/"
+    final=`echo $line | sed -e "$sedline" | sed -e "s/% \"/%\"/"`
+
+    echo "$final" || exit 1
 done
