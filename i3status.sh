40fdc005ad1ead480a763234a95ffdc1d8cd1158
Gordin <9ordin@gmail.com>
2013-05-11 23:02:35 +0200

    formatting

--- i3status.sh
+++ i3status.sh
@@ -11,49 +11,46 @@ i3status | while :
 do
     read line
 
-    if [ `cat /sys/class/net/eth0/operstate` == "up" ]
-        then
-            ETHR2=`cat /sys/class/net/eth0/statistics/rx_bytes`
-            ETHT2=`cat /sys/class/net/eth0/statistics/tx_bytes`
-            ETHTBPS=`expr $ETHT2 - $ETHT1`
-            ETHRBPS=`expr $ETHR2 - $ETHR1`
-            ETHTKBPS=`expr $ETHTBPS / 1024`
-            ETHRKBPS=`expr $ETHRBPS / 1024`
+    if [ `cat /sys/class/net/eth0/operstate` == "up" ]; then
+        ETHR2=`cat /sys/class/net/eth0/statistics/rx_bytes`
+        ETHT2=`cat /sys/class/net/eth0/statistics/tx_bytes`
+        ETHTBPS=`expr $ETHT2 - $ETHT1`
+        ETHRBPS=`expr $ETHR2 - $ETHR1`
+        ETHTKBPS=`expr $ETHTBPS / 1024`
+        ETHRKBPS=`expr $ETHRBPS / 1024`
 
-            ETHR1=$ETHR2
-            ETHT1=$ETHT2
-            sedeth="s/\(\"name\":\"ethernet\",\"instance\":\"eth0\".*)\)\(\"}\)/\1 D:$ETHRKBPS U:$ETHTKBPS\2/"
-            line=`echo $line | sed -e "$sedeth"`
+        ETHR1=$ETHR2
+        ETHT1=$ETHT2
+        sedeth="s/\(\"name\":\"ethernet\",\"instance\":\"eth0\".*)\)\(\"}\)/\1 D $ETHRKBPS U $ETHTKBPS\2/"
+        line=`echo $line | sed -e "$sedeth"`
     fi
 
-    if [ `cat /sys/class/net/usb0/operstate` == "up" ]
-        then
-            USBR2=`cat /sys/class/net/usb0/statistics/rx_bytes`
-            USBT2=`cat /sys/class/net/usb0/statistics/tx_bytes`
-            USBTBPS=`expr $USBT2 - $USBT1`
-            USBRBPS=`expr $USBR2 - $USBR1`
-            USBTKBPS=`expr $USBTBPS / 1024`
-            USBRKBPS=`expr $USBRBPS / 1024`
+    if [ `cat /sys/class/net/usb0/operstate` == "up" ]; then
+        USBR2=`cat /sys/class/net/usb0/statistics/rx_bytes`
+        USBT2=`cat /sys/class/net/usb0/statistics/tx_bytes`
+        USBTBPS=`expr $USBT2 - $USBT1`
+        USBRBPS=`expr $USBR2 - $USBR1`
+        USBTKBPS=`expr $USBTBPS / 1024`
+        USBRKBPS=`expr $USBRBPS / 1024`
 
-            USBR1=$USBR2
-            USBT1=$USBT2
-            sedusb="s/\(\"name\":\"ethernet\",\"instance\":\"usb0\".*)\)\(\"}\)/\1 D:$USBRKBPS U:$USBTKBPS\2/"
-            line=`echo $line | sed -e "$sedusb"`
+        USBR1=$USBR2
+        USBT1=$USBT2
+        sedusb="s/\(\"name\":\"ethernet\",\"instance\":\"usb0\".*)\)\(\"}\)/\1 D $USBRKBPS U $USBTKBPS\2/"
+        line=`echo $line | sed -e "$sedusb"`
     fi
 
-    if [ `cat /sys/class/net/wlan0/operstate` == "up" ]
-        then
-            WLANR2=`cat /sys/class/net/wlan0/statistics/rx_bytes`
-            WLANT2=`cat /sys/class/net/wlan0/statistics/tx_bytes`
-            WLANTBPS=`expr $WLANT2 - $WLANT1`
-            WLANRBPS=`expr $WLANR2 - $WLANR1`
-            WLANTKBPS=`expr $WLANTBPS / 1024`
-            WLANRKBPS=`expr $WLANRBPS / 1024`
+    if [ `cat /sys/class/net/wlan0/operstate` == "up" ]; then
+        WLANR2=`cat /sys/class/net/wlan0/statistics/rx_bytes`
+        WLANT2=`cat /sys/class/net/wlan0/statistics/tx_bytes`
+        WLANTBPS=`expr $WLANT2 - $WLANT1`
+        WLANRBPS=`expr $WLANR2 - $WLANR1`
+        WLANTKBPS=`expr $WLANTBPS / 1024`
+        WLANRKBPS=`expr $WLANRBPS / 1024`
 
-            WLANR1=$WLANR2
-            WLANT1=$WLANT2
-            sedwlan="s/\(\"name\":\"wireless\",\"instance\":\"wlan0\".*)\)\(\"}\)/\1 D:$WLANRKBPS U:$WLANTKBPS\2/"
-            line=`echo $line | sed -e "$sedwlan"`
+        WLANR1=$WLANR2
+        WLANT1=$WLANT2
+        sedwlan="s/\(\"name\":\"wireless\",\"instance\":\"wlan0\".*)\)\(\"}\)/\1 D $WLANRKBPS U $WLANTKBPS\2/"
+        line=`echo $line | sed -e "$sedwlan"`
     fi
 
     # get free and used RAM (this excludes cache)
